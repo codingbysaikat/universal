d3e437412c68876fc5381e6864ec770f6107b9f3
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-08-27 01:03:18 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ let menu = document.querySelector('.menu');
 document.querySelector('#menu-btn').onclick = () =>{
     menu.classList.toggle('active');
 }
+window.onscroll = () =>{
+    if(menu.classList.contains('active')){
+        menu.classList.remove('active');
+
+    }
+}
